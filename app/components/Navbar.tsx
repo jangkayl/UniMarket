2fd798fdbc980f3fddb9ac42d7f853ec80942b22
drew <ralphandrew2004@gmail.com>
2025-12-03 14:23:58 +0800

    made logo link clicable, directs to landing page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <header className="bg-red-900 text-white py-4 px-8 flex justify-between items-center w-full sticky top-0">
-      {/* Logo Section - Now a clickable Link */}
       <Link
         href="/"
         className="text-2xl font-semibold flex items-center italic"
